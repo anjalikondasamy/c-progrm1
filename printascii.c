59db4986200bbd6c058674381297dd3d015cd7bd
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-28 16:09:32 +0530

    printascii.c
    good

---
+++ printascii.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("\n enter the character:");
+scanf("%d",ch);
+printf("the value of character %c=%d"ch,ch);
+getch();
+return 0;
+}
